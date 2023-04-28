ff7475539eb1385a52e654a88dd25851a4807489
CAUE <caue.ms01@gmail.com>
2023-04-28 15:15:10 -0300

    whops

--- src/music/entity/music.entity.ts
+++ src/music/entity/music.entity.ts
@@ -1,25 +1,26 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, ManyToMany } from 'typeorm';
 import { AlbumEntity } from '../../album/entity/album.entity';
 import { PlaylistEntity } from '../../playlist/entity/playlist.entity';
+import { Length } from 'class-validator';
 
 @Entity({name:'MUSIC'})
 export class MusicEntity {
   @PrimaryGeneratedColumn({name:'ID_MUSIC'})
   id: number;
 
-  @Column({name:'NAME'})
+  @Column({name:'NAME',length:50,nullable:false})
   name: string;
 
-  @Column({name:'PHOTO_PATH'})
+  @Column({name:'PHOTO_PATH',length:50,nullable:false})
   photo: string;
 
-  @Column({name:'COMPOSER'})
+  @Column({name:'COMPOSER',length:50,nullable:false})
   composer: string;
 
-  @Column()
+  @Column({name:'TIMESTAMP',length:50,nullable:false})
   timestamp: string;
 
-  @Column()
+  @Column({name:'MUSIC_PATH',length:50,nullable:false})
   path: string;
 
   @ManyToOne(() => AlbumEntity, album => album.music)
